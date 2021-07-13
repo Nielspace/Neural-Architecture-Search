979a1ff25e39fe2cedadf2217537bff954d14b08
Nilesh Barla <nielspace26@gmail.com>
2021-07-13 23:53:37 +0530

    Update README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Neural Architecture Search
+
+This repository contains codes for NAS and AutoML. 
+
+Core Library: Pytorch
--- basicNAS/nas.py
+++ basicNAS/nas.py
@@ -68,7 +68,7 @@ class NAS():
 
     def count_parameters(self,model):
         total_params = 0
-        for name, parameter in model.named_parameters():
+        for _, parameter in model.named_parameters():
             if not parameter.requires_grad: continue
             param = parameter.numel()
             total_params+=param
